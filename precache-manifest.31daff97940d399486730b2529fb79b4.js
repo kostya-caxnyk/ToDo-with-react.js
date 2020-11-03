0b72b48b909f60c78201ebd13c0e633b027a9a46
kostya <caxnyk2002@gmail.com>
2020-11-03 11:24:12 +0300

    Updates

---
+++ precache-manifest.31daff97940d399486730b2529fb79b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8591d6b3e2b0f555a42d06d4048807e2",
+    "url": "/ToDo-with-react.js/index.html"
+  },
+  {
+    "revision": "2bf0045b7d6ce97ca6ae",
+    "url": "/ToDo-with-react.js/static/css/main.8f0028df.chunk.css"
+  },
+  {
+    "revision": "3839c8c7f1926c97171e",
+    "url": "/ToDo-with-react.js/static/js/2.47dd38e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-with-react.js/static/js/2.47dd38e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bf0045b7d6ce97ca6ae",
+    "url": "/ToDo-with-react.js/static/js/main.11221b4c.chunk.js"
+  },
+  {
+    "revision": "52ad5a2d498d84bc00d8",
+    "url": "/ToDo-with-react.js/static/js/runtime-main.49b6718d.js"
+  }
+]);
